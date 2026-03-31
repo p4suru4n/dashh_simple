682b735e3d6f61686bf98e25e3387534d881ad9d
p4suru4n <wbpasuruan@gmail.com>
2026-03-31 10:57:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let ip_ccms = [
 
 async function refreshStatus() {
   const res = await fetch(
-    "https://vhs-evaluated-source-recording.trycloudflare.com/api/status",
+    "https://presentation-zen-llp-alerts.trycloudflare.com/api/status",
     // "/api/status",
   );
   // const res = await fetch("/api/status");
