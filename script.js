8ab2e576caf35dbda3452a3fd93a866228c6f8ee
p4suru4n <wbpasuruan@gmail.com>
2026-04-16 11:11:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let ip_ccms = [
 
 async function refreshStatus() {
   const res = await fetch(
-    "https://equally-forests-runner-wider.trycloudflare.com/api/status",
+    "https://sage-sparc-neutral-superior.trycloudflare.com/api/status",
     // "/api/status",
   );
   // const res = await fetch("/api/status");
