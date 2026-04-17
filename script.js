a43d14be374e9a8577b14e6f15e967683f51890b
p4suru4n <wbpasuruan@gmail.com>
2026-04-17 11:49:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let ip_ccms = [
 
 async function refreshStatus() {
   const res = await fetch(
-    "https://sage-sparc-neutral-superior.trycloudflare.com/api/status",
+    "https://drag-media-finding-equations.trycloudflare.com/api/status",
     // "/api/status",
   );
   // const res = await fetch("/api/status");
