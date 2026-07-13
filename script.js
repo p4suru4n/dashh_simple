ff8e9d3c6a0a583f58a0510a43c4c0ba49f6059c
p4suru4n <wbpasuruan@gmail.com>
2026-07-13 09:01:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let ip_ccms = [
 
 async function refreshStatus() {
   const res = await fetch(
-    "https://sole-photo-jungle-household.trycloudflare.com/api/status",
+    "https://spine-rice-drinks-dot.trycloudflare.com/api/status",
     // "/api/status",
   );
   // const res = await fetch("/api/status");
