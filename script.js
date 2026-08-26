9facbf35e39fdaf19bcb1bf33b3921b141e0c37c
p4suru4n <wbpasuruan@gmail.com>
2026-08-26 13:09:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let ip_ccms = [
 
 async function refreshStatus() {
   const res = await fetch(
-    "https://forward-rank-flows-cinema.trycloudflare.com/api/status",
+    "https://missing-solving-veterans-compound.trycloudflare.com/api/status",
     // "/api/status",
   );
   // const res = await fetch("/api/status");
